af27262e4bbd0149fbe5aba08a267eaa4c96ab0d
Harsonica <72625830+Harsonica@users.noreply.github.com>
2021-04-14 22:44:37 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,30 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Text, TextInput } from 'react-native';
+import {Header} from 'react-native-elements';
+import homeScreen from '../homeScreen';
+export default class HomeScreen extends Component
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  inputBoxContainer:{
+    flex:0.3,
+    alignItems:'center'
+  },
+  inputBox:{
+    width:'80%',
+    alignSelf:'center',
+    height:40,
+  }
+   
+});
